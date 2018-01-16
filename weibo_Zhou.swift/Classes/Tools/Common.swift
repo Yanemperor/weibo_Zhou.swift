0f4cb9e58079c62373b04ddc7265f0f32d639f88
Zhou <zhouzilong09@163.com>
2018-01-16 16:59:08 +0800

    up token

--- weibo_Zhou.swift/Classes/Tools/Common.swift
+++ weibo_Zhou.swift/Classes/Tools/Common.swift
@@ -22,7 +22,7 @@ public let kAppRedirectURI = "https://api.weibo.com/oauth2/default.html"
 
 //宏定义
 public let kUserDefaults = UserDefaults.standard
-public let kWBToken = "2.00KDEKPGyY87OCf2a7141c01mHvkCD"
+public let kWBToken = "2.00KDEKPGyY87OCa561f3fdc02kxVCE"
 public let kWBUserID = "uid"
 public let kWBRefreshToken = "wbRefreshToken"
 public var kisLogin = UserData.shared.isLongin
